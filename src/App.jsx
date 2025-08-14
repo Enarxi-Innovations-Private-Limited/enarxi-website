366bed931a99e1c162cd670af4991f81e0ae306b
Asyedabdulrahman <asyedabdulrahman3@gmail.com>
2025-08-14 15:40:34 +0530

    syed worked on frontend by using ARIA principles

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
+import Header from "./components/Header";
 
 function App() {
-
   return (
     <>
-      <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <Header />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
