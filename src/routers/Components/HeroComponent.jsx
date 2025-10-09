a977bb8aee658531bc39652533d0e87c96ee7200
Asyedabdulrahman3 <asyedabdulrahman3@gmail.com>
2025-10-09 17:57:40 +0530

    update: worked on scrolltoTop

--- src/routers/Components/HeroComponent.jsx
+++ src/routers/Components/HeroComponent.jsx
@@ -3,7 +3,7 @@ import heroVideo from "../../assets/Videos/chroma-keyed-video.webm";
 const HeroComponent = () => {
   return (
     <section className="w-full h-[calc(90vh-64px)] bg-gradient-to-b from-[#DEF4FF] to-white flex flex-col items-center justify-center text-center px-4">
-      <p className="font-noto font-medium leading-[100%] tracking-[0.78px] text-[#1840A7] text-center capitalize text-2xl sm:text-3xl md:text-3xl lg:4xl mb-4 mt-6">
+      <p className="font-noto font-medium leading-[100%] tracking-[0.78px] text-[#1840A7] text-center capitalize text-2xl sm:text-3xl md:text-3xl lg:4xl mb-4 mt-24">
         Don't Just Dream It, Build It
       </p>
 
@@ -13,7 +13,7 @@ const HeroComponent = () => {
 
       {/* 🛠️ MODIFIED VIDEO CONTAINER AND VIDEO ELEMENT 🛠️ */}
       <div className="mt-6 w-full md:w-2/5 lg:w-1/3 md:h-auto">
-  <video
+         <video
     src={heroVideo}
     autoPlay
     loop
@@ -21,7 +21,8 @@ const HeroComponent = () => {
     playsInline
     className="w-full h-full object-cover"
   />
-</div>    </section>
+    </div>   
+ </section>
   );
 };
 
