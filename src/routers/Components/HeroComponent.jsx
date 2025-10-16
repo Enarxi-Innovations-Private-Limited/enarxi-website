0c48c46d8d6d0303f84f4eaf864fd1782fcf252d
chella-ubun@asus <chellakumar.pr@gmail.com>
2025-10-16 15:22:28 +0530

    update: hero section re-organized to be little top

--- src/routers/Components/HeroComponent.jsx
+++ src/routers/Components/HeroComponent.jsx
@@ -3,12 +3,12 @@ import heroVideo from "../../assets/Videos/chroma-keyed-video.webm";
 const HeroComponent = () => {
   return (
     <section className="w-full h-[calc(90vh-64px)] bg-gradient-to-b from-[#DEF4FF] to-white flex flex-col items-center justify-center text-center px-4">
-      <p className="font-noto font-medium leading-[100%] tracking-[0.78px] text-[#1840A7] text-center capitalize text-2xl sm:text-3xl md:text-3xl lg:4xl mb-4 mt-24">
-        Don't Just Dream It, Build It
+      <p className="font-oswald text-weight-600 leading-[100%] tracking-[0.78px] text-[#1840A7] text-center capitalize text-2xl sm:text-3xl md:text-3xl lg:4xl mb-4 mt-15">
+        Don’t just dream — build it
       </p>
 
       <h1 className="[font-weight:600] text-[#0A1524] max-w-3xl text-lg sm:text-xl md:text-[34px] font-poppins capitalize mb-6">
-        Your imagination knows nobounds. Let's make something amazing together!
+        Limitless imagination. Endless possibilities. Let’s build together!
       </h1>
 
       {/* 🛠️ MODIFIED VIDEO CONTAINER AND VIDEO ELEMENT 🛠️ */}
